0a13d1c4781456d76d447261f1b8762eeed3ded0
Eduard Likwong <ejllikwong@gmail.com>
2020-05-13 17:29:05 +0800

    Added CodeForces to ProgrammingProblems, Added solution to problem Watermelon in CodeForces

--- LeetCode/April/MaxSubarray/solution.cpp
+++ LeetCode/April/MaxSubarray/solution.cpp
@@ -1,16 +1,19 @@
-class Solution
-{
-public:
-    int maxSubArray(vector<int> &nums)
-    {
-        int ans = INT_MIN, sub = 0;
-        for (int x : nums)
-        {
-            sub += x;
-            ans = max(ans, sub);
-            sub = max(sub, 0);
-        }
+#include <stdio.h>
+#include <vector>
+using namespace std;
 
-        return ans;
+int maxSubArray(vector<int> &nums)
+{
+    int ans = INT_MIN, sub = 0;
+    for (int x : nums)
+    {
+        sub += x;
+        ans = max(ans, sub);
+        sub = max(sub, 0);
     }
-};
+    return ans;
+}
+
+int main()
+{
+}
